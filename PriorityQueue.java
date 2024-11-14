a87d0dce220a252e9cfc2fc688fe70dc02dc58dd
Mary Margaret <83988411+msipes1@users.noreply.github.com>
2024-11-13 22:31:06 -0600

    Update PriorityQueue.java

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,7 +1,7 @@
 
 /******************************************************************
  *
- *   YOUR NAME / SECTION NUMBER
+ *   Mimi Sipes / COMP 272-002
  *
  *   Note, additional comments provided throughout this source code
  *   is for educational purposes
